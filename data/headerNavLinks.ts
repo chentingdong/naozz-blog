5ff0501270c519d89672651ef9781ca816d797ea
Tingdong Chen <chentingdong@gmail.com>
2024-11-21 00:50:15 -0800

    spin out to submodule

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,7 @@
+const headerNavLinks = [
+  { href: 'https://naozz.com', title: 'Home' },
+  { href: 'https://blog.naozz.com', title: 'Blog' },
+  { href: 'https://blog.naozz.com/about', title: 'About' },
+]
+
+export default headerNavLinks
