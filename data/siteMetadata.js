f6a52513a4dc738f53e09e7e64cbb7da07d17041
Tingdong Chen <chentingdong@gmail.com>
2024-11-21 11:32:17 -0800

    post comments working

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -72,7 +72,7 @@ const siteMetadata = {
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'dark',
+      theme: 'system',
       // theme when dark mode
       darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
